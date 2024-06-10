fe602a3bbc606dc9f496d79da8ed305ec020bfa1
ollia008 <97535630+ollia008@users.noreply.github.com>
2024-06-10 09:16:01 +0200

    Set up and starting to use ExpressJs

---
+++ routes/users.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('User List')
+})
+
+router.get('/new', (req, res) => {
+    res.send('User New Form')
+})
+
+router.post('/', (req, res) => {
+    res.send('Create User')
+})
+
+
+router
+.route('/:id').get((req, res) => {
+    res.send(`Get User With ID ${req.params.id}`)
+})
+.put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`)
+})
+
+const users = [{ name: "Alf" }, { name: "Per" }]
+
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    // console.log(id)
+    next()
+})
+
+module.exports = router
+
